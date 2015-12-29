f954f54478f2d6c2395ecd478d6a25b758eb1da9
huang <18751979220@163.com>
2015-12-29 15:30:33 +0800

    hao hao hao

---
+++ testtt/testtt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testtt
+//
+//  Created by tarena on 15/12/29.
+//  Copyright © 2015年 huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testtt/testtt/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  testtt
+//
+//  Created by tarena on 15/12/29.
+//  Copyright © 2015年 huang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"sdcsadsafdasf");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
